22c5f8c915bf1a5b6ec33748b71fb2826e45479d
DimasiK <testphp00@mail.ru>
2017-05-25 21:17:22 +1000

    Added form

---
+++ views/form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Новостей нет</h1>
+	<form action="/../addnews.php" method="post">
+		<p>
+			Заголовок:<br>
+			<input type="text" name="title" size="35">
+		</p>
+		<p>
+			Текст:<br>
+			<textarea name="text" cols="40" rows="4"></textarea>
+		</p>
+		<input class="sub" type="submit" value="Отправить">
+	</form>
+</body>
+</html>
