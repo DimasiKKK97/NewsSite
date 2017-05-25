d8598a3584be6ce39500d16af722094d186c82b0
DimasiK <testphp00@mail.ru>
2017-05-25 20:59:37 +1000

    Fixed controller

--- addnews.php
+++ addnews.php
@@ -1,20 +1,21 @@
 <?php 
 	
-	require '/models/news_class.php';
+require __DIR__ . '/models/news_class.php';
 
-	if(empty($_POST['title']) || empty($_POST['text'])){
-		header('Location: index.php');
-		exit;
-	}
-	
-	$title = $_POST['title'];
-	$text = $_POST['text'];
+if(empty($_POST['title']) || empty($_POST['text'])){
+	header('Location: index.php');
+	exit;
+}
 
-	$addnew = new News();
-	$addnew->addNews($title, $text);
+$title = $_POST['title'];
+$text = $_POST['text'];
 
-	unset($_POST['title']);
-	unset($_POST['text']);
+$addnew = new News();
+$addnew->addNews($title, $text);
 
-	header('Location: /index.php');
+unset($_POST['title']);
+unset($_POST['text']);
+
+header('Location: /index.php');
+
 ?>
